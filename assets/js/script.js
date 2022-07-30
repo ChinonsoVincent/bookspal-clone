404b1dfff81071e3b980e45a7bb18e4824474cba
Chinonso Vincent <nchinonsovincent@gmail.com>
2022-07-30 12:19:13 +0100

    fixed book gallery section, FAQ section and footer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,19 @@
-$(".owl-carousel").owlCarousel({
+var owl = $(".owl-carousel");
+owl.owlCarousel({
 	loop: true,
 	margin: 10,
-	nav: true,
+	autoplay: true,
+	autoplayTimeout: 1000,
+	autoplayHoverPause: true,
 	responsive: {
 		0: {
-			items: 1,
-		},
-		600: {
 			items: 2,
 		},
-		1000: {
+		600: {
 			items: 3,
+		},
+		1000: {
+			items: 5,
 		},
 	},
 });
